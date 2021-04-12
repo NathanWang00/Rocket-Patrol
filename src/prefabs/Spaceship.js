fc00a25b41386c3144959db22ca3fd2d28100fa0
Nathan Wang <nxwang@ucsc.edu>
2021-04-11 18:35:08 -0700

    Spaceship move

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,20 @@
+// Ship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene, x, y, texture, frame);
+  
+    // add object to existing scene
+    scene.add.existing(this);
+    this.points = pointValue;
+    this.moveSpeed = 3;
+    }
+
+    update() {
+        //move ship left
+        this.x -= this.moveSpeed;
+        //wrap around from left to right
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
