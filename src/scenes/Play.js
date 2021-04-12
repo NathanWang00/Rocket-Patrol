1805d5b522c4839214b7bea3221b7c7bcc6bae70
Nathan Wang <nxwang@ucsc.edu>
2021-04-11 21:53:10 -0700

    Menu Difficulty and Sound

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -72,9 +72,9 @@ class Play extends Phaser.Scene {
 
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
 
@@ -86,6 +86,9 @@ class Play extends Phaser.Scene {
         // check key input for restart
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
         }
 
         this.starfield.tilePositionX -= 10;
@@ -139,7 +142,6 @@ class Play extends Phaser.Scene {
         this.scoreLeft.text = this.p1Score;
         
         this.explosion.play();
-        //this.sound.play('sfx_explosion');
     }
 
 }
