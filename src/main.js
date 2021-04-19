3fe9488b7e3c7c019a7a1e31462ddab4ad8cc71c
Nathan Wang <nxwang@ucsc.edu>
2021-04-18 21:44:55 -0700

    typo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //Nathan Wang, Rocket Patrol Extreme, 4/19/21, ~10 hours for the mod, ~10 hours for the base game.
 //BGM (False Awakenings) by https://soundcloud.com/anttu-janhunen/
-//Points: random ship movement (5), speed up after 30 sec (5), bgm (5) parallax scrolling (10), remaining time (10), add time for successful hit (20), mouse controls (20)
+//Points: random ship movement (5), speed up after 30 sec (5), bgm (5), parallax scrolling (10), remaining time (10), add time for successful hit (20), mouse controls (20)
 //spaceship variant (20)
 //Total: 95
 //Not sure if the background changes counted for any points 
